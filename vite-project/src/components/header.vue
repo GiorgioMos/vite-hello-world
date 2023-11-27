edfe41a0a182eda9dd4f24da0ae8ed0f3f92eb5d
GiorgioMos <giorgiomosca2@gmail.com>
2023-11-27 18:26:06 +0100

    First commit

---
+++ vite-project/src/components/header.vue
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<template>
+    <h1>Benvenuto in immagine carina 🛴</h1>
+</template>
+
+<style>
+h1 {
+    font-family: sans-serif;
+}
+</style>
---
+++ vite-project/src/components/jumbo.vue
@@ -0,0 +1,17 @@
+<!-- java -->
+<script>
+</script>
+
+<!-- HTML -->
+<template>
+    <img src="https://img.freepik.com/premium-vector/cute-sleep-sloth-cartoon-with-cute-pose-cartoon-icon-illustration-animal-icon-concept-pink-background_244307-393.jpg?w=826"
+        alt="">
+</template>
+
+<!-- CSS -->
+<style>
+img {
+    width: 400px;
+    border-radius: 10px;
+}
+</style>
